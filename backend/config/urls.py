31ce8d1a23803397c5126b4a427e90cbd2008045
Ashif EK <ashifek11@gmail.com>
2026-02-02 16:20:21 +0530

    feat: Implement Razorpay payment processing and configure API endpoints.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,3 +18,7 @@ urlpatterns = [
 urlpatterns += [
     re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/payments/services.py
+++ backend/payments/services.py
@@ -1,21 +1,17 @@
 import razorpay
 from django.conf import settings
 
-client = razorpay.Client(
-    auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET)
-)
+client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+
 
 def create_razorpay_order(order):
-    if not order.total:
+    if not order.total_amount:
         raise ValueError("Order total is missing")
 
-    amount = int(float(order.total) * 100)
+    amount = int(float(order.total_amount) * 100)
 
-    razorpay_order = client.order.create({
-        "amount": amount,
-        "currency": "INR",
-        "payment_capture": 1
-    })
+    razorpay_order = client.order.create(
+        {"amount": amount, "currency": "INR", "payment_capture": 1}
+    )
 
     return razorpay_order
-
